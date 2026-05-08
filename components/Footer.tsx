6b15fd4a3e6bb10237a21f95db89d7e628716654
Riverr360 Consulting <riverr360consulting@gmail.com>
2026-05-08 23:48:49 +0530

    fix:footer of get started page fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Footer() {
+  const pathname = usePathname();
+  if (pathname === '/get-started') return null;
+
   const currentYear = new Date().getFullYear();
 
   return (
