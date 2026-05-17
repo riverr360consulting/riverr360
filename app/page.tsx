81411c9c2cadcd1a3b3316e58e5c2642ff7999be
Riverr360 Consulting <riverr360consulting@gmail.com>
2026-05-17 14:48:25 +0530

    fix: remove duplicate framework sections from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from '@/components/HeroSection';
 import ProblemsSection from '@/components/ProblemsSection';
 import FrameworkDiagram from '@/components/FrameworkDiagram';
-import LeakageExplained from '@/components/LeakageExplained';
-import ReasonsSection from '@/components/ReasonsSection';
 import SolutionsSection from '@/components/SolutionsSection';
 import WebinarHomeSection from '@/components/WebinarHomeSection';
 
@@ -12,8 +10,6 @@ export default function HomePage() {
       <HeroSection />
       <ProblemsSection />
       <FrameworkDiagram />
-      <LeakageExplained />
-      <ReasonsSection />
       <SolutionsSection />
       <WebinarHomeSection />
     </>
